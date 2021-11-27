747432ac803dbb443422dc94b4ff9f42200eecba
Rajinderpal Singh <60106112+sairish2001@users.noreply.github.com>
2021-11-27 19:00:54 +0530

    Added experiences and Skills

--- js/data.js
+++ js/data.js
@@ -1,52 +1,192 @@
-var projectsdata =
-{
-    "c/c++":
-        [
-            { "name": "My Shop Management", "videolink": "https://user-images.githubusercontent.com/60106112/106363902-60a72680-6351-11eb-98a6-1e218e94c3d4.gif", "description": ["OPP","File Handling"], "date": "January,2020", "githubrepo": "https://github.com/sairish2001/My-Shop-Management" }
-        ]
-    ,
-    "python":
-        [
-            { "name": "Snake Game", "videolink": "https://user-images.githubusercontent.com/60106112/106362884-4702e080-634b-11eb-9881-ba65a90f4fa1.gif", "description": ["pygame module", "OOP", "SQL Backend for maintaining HighScore"], "date": "November,2019", "githubrepo": "https://github.com/sairish2001/Snake-Game-using-Python" },
-            { "name": "Login/SignUp Setup", "videolink": "https://user-images.githubusercontent.com/60106112/106384647-f2ff0700-63f1-11eb-82af-5cd3f843ad00.mp4", "description": ["GUI using tkinter module", "OOP", "SQL Backend", "OTP Verification after Account Creation through E-mail", "Send Friend Requests", "Many More"], "date": "February-March,2020", "githubrepo": "https://github.com/sairish2001/Login-Signup-Setup" },
-            { "name": "TIC-TAC-TOE", "videolink": "https://user-images.githubusercontent.com/60106112/106386217-f39b9b80-63f9-11eb-95b3-ea9b9bdc226d.mp4", "description": ["GUI using tkinter module","OOP"], "date": "May,2020", "githubrepo": "https://github.com/sairish2001/TIC-TAC-TOE-in-Python" },
-            { "name": "Scientific Calculator", "videolink": "https://user-images.githubusercontent.com/60106112/106387270-0b295300-63ff-11eb-8288-f472c8569be8.mp4", "description": ["GUI using tkinter module","OOP","Some Intelligence Working,Checks","General Operations","Trignometry + Inverse(Degree and Radians)"], "date": "May,2020", "githubrepo": "https://github.com/sairish2001/Scientific-Calculator" },
-        ]
-    ,
-    "django":
-        [
-            { "name": "Shopping Website", "videolink": null, "description": [`Admin:<ul><li>Add, Delete, Edit Products</li><li>Manage Shipping and Payments</li></ul>`,`User:<ul><li>View, Buy Products</li><li>Add to Cart, make Payment</li></ul>`], "date": "July,2020 - August,2021", "githubrepo": "https://github.com/sairish2001/Shopping-Site-Django" }            
-        ]
-    ,
-    "htmlcssjs":
-        [
-            { "name": "TIC-TAC-TOE", "videolink": "https://user-images.githubusercontent.com/60106112/106387578-5d1ea880-6400-11eb-9e09-612025575b2d.mp4", "description": ["Bootstrap","JavaScript"], "date": "June,2020", "githubrepo": "https://github.com/sairish2001/TIC-TAC-TOE-using-JavaScript" },
-            { "name": "Makes Math Easy", "videolink": "https://user-images.githubusercontent.com/60106112/106388700-c6ed8100-6405-11eb-9ca2-a872eae89d4d.mp4", "description": [`Part of <a href="https://gssoc.girlscript.tech/projects.html">GSSOC'21</a> Open Source Program`,"Step By Step Solutions for Various Maths Problems","Scripted in JavaScript","KaTex for Displaying Maths","Bootstrap"], "date": "July,2020", "githubrepo": "https://github.com/makesmatheasy/makesmatheasy" },
-            { "name": "iWasAt", "videolink": null, "description": ["Contributed in Winter of Code Event","Generates Frames for events","Bootstrap"], "date": "December,2020 - January,2021", "githubrepo": "https://github.com/sairish2001/omg-frames" },
-        ]
-}
+var projectsdata = {
+  "c/c++": [
+    {
+      name: "My Shop Management",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106363902-60a72680-6351-11eb-98a6-1e218e94c3d4.gif",
+      description: ["OPP", "File Handling"],
+      date: "January,2020",
+      githubrepo: "https://github.com/sairish2001/My-Shop-Management",
+    },
+  ],
+  python: [
+    {
+      name: "Snake Game",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106362884-4702e080-634b-11eb-9881-ba65a90f4fa1.gif",
+      description: [
+        "pygame module",
+        "OOP",
+        "SQL Backend for maintaining HighScore",
+      ],
+      date: "November,2019",
+      githubrepo: "https://github.com/sairish2001/Snake-Game-using-Python",
+    },
+    {
+      name: "Login/SignUp Setup",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106384647-f2ff0700-63f1-11eb-82af-5cd3f843ad00.mp4",
+      description: [
+        "GUI using tkinter module",
+        "OOP",
+        "SQL Backend",
+        "OTP Verification after Account Creation through E-mail",
+        "Send Friend Requests",
+        "Many More",
+      ],
+      date: "February-March,2020",
+      githubrepo: "https://github.com/sairish2001/Login-Signup-Setup",
+    },
+    {
+      name: "TIC-TAC-TOE",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106386217-f39b9b80-63f9-11eb-95b3-ea9b9bdc226d.mp4",
+      description: ["GUI using tkinter module", "OOP"],
+      date: "May,2020",
+      githubrepo: "https://github.com/sairish2001/TIC-TAC-TOE-in-Python",
+    },
+    {
+      name: "Scientific Calculator",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106387270-0b295300-63ff-11eb-8288-f472c8569be8.mp4",
+      description: [
+        "GUI using tkinter module",
+        "OOP",
+        "Some Intelligence Working,Checks",
+        "General Operations",
+        "Trignometry + Inverse(Degree and Radians)",
+      ],
+      date: "May,2020",
+      githubrepo: "https://github.com/sairish2001/Scientific-Calculator",
+    },
+  ],
+  django: [
+    {
+      name: "Shopping Website",
+      videolink: null,
+      description: [
+        `Admin:<ul><li>Add, Delete, Edit Products</li><li>Manage Shipping and Payments</li></ul>`,
+        `User:<ul><li>View, Buy Products</li><li>Add to Cart, make Payment</li></ul>`,
+      ],
+      date: "July,2020 - August,2021",
+      githubrepo: "https://github.com/sairish2001/Shopping-Site-Django",
+    },
+  ],
+  htmlcssjs: [
+    {
+      name: "TIC-TAC-TOE",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106387578-5d1ea880-6400-11eb-9e09-612025575b2d.mp4",
+      description: ["Bootstrap", "JavaScript"],
+      date: "June,2020",
+      githubrepo: "https://github.com/sairish2001/TIC-TAC-TOE-using-JavaScript",
+    },
+    {
+      name: "Makes Math Easy",
+      videolink:
+        "https://user-images.githubusercontent.com/60106112/106388700-c6ed8100-6405-11eb-9ca2-a872eae89d4d.mp4",
+      description: [
+        `Part of <a href="https://gssoc.girlscript.tech/projects.html">GSSOC'21</a> Open Source Program`,
+        "Step By Step Solutions for Various Maths Problems",
+        "Scripted in JavaScript",
+        "KaTex for Displaying Maths",
+        "Bootstrap",
+      ],
+      date: "July,2020",
+      githubrepo: "https://github.com/makesmatheasy/makesmatheasy",
+    },
+    {
+      name: "iWasAt",
+      videolink: null,
+      description: [
+        "Contributed in Winter of Code Event",
+        "Generates Frames for events",
+        "Bootstrap",
+      ],
+      date: "December,2020 - January,2021",
+      githubrepo: "https://github.com/sairish2001/omg-frames",
+    },
+  ],
+};
 
 var skillsdata = {
-    'Full Stack Web': 90,
-    'Machine Learning': 70,
-}
+  "Full Stack Web": 90,
+  "Machine Learning": 70,
+};
 
 var techdata = {
-    'SQL': 'images/techlogos/sql.png',
-    'Django': 'images/techlogos/django.png',
-    'Python': 'images/techlogos/python.png',
-    'JavaScript': 'images/techlogos/JavaScript.png',    
-    'CSS3':'images/techlogos/css.png',
-    'HTML5':'images/techlogos/html.png',
-    'Bootstrap':'images/techlogos/bootstrap.png',
-    'REST API':'images/techlogos/restapi.jpg',
-    'Video/Image Editing':'images/techlogos/video.jpg'
-}
+  SQL: "images/techlogos/sql.png",
+  Django: "images/techlogos/django.png",
+  Python: "images/techlogos/python.png",
+  JavaScript: "images/techlogos/JavaScript.png",
+  CSS3: "images/techlogos/css.png",
+  HTML5: "images/techlogos/html.png",
+  Bootstrap: "images/techlogos/bootstrap.png",
+  "REST API": "images/techlogos/restapi.jpg",
+  "Video/Image Editing": "images/techlogos/video.jpg",
+  Flutter: "images/techlogos/flutter.jpg",
+};
 
-var experiencedata = [
-    { "position": "Google Developer Student Club Lead", "companyname":"Google Developers", "companyurl": null, "date": "August,2021 - Present", "description": ["Empower Students under Google DSC Lead", "Organize Workshops,Events, etc."] },
-    { "position": "Hacktoberfest 2020", "companyname":"Hacktoberfest", "companyurl": "images/certificates/hacktoberfestpullrequests.jpg", "date": "October,2020-October,2020", "description": ["Contributed First time in Open Source","Merged 4 Pull Requests"] },
-    { "position": "Winter of Code 2020", "companyname":"Winter of Code,2020", "companyurl": "images/certificates/winterofcode.jpg", "date": "December,2020-January,2021", "description": ["Contributed to Open Source Project","OMG Frames","Became TOP Contributor"] },
-    { "position": "HackerRank", "companyname":"HackerRank", "companyurl": "images/certificates/hackerraank.jpg", "date": "July,2020", "description": ["Cleared HackerRank Python(Basic Skill Test)"] },
-    { "position": "GirlScript Summer of Code,2021", "companyname":"GirlScript Summer of Code", "companyurl": "images/certificates/girlscriptcertificate.png", "date": "March,2021-Present", "description": ["Project Admin",`<a href="https://github.com/sairish2001/makesmatheasy.github.io">Repository</a>`] },
-]
+var experiencedata = [    
+  {
+    position: "Google Developer Student Club Lead",
+    companyname: "Google",
+    companyurl: null,
+    date: "August,2021 - Present",
+    description: [
+      "Empower Students",
+      "Organize Workshops,Events, etc.",
+    ],
+  },
+  {
+    position: "Hacktoberfest 2021",
+    companyname: "Hacktoberfest",
+    companyurl: null,
+    date: "October,2021-October,2021",
+    description: [
+      "Celebrated OpenSource with it",
+      "Merged 4 Pull Requests",
+    ],
+  },
+  {
+    position: "Winter of Code 2020",
+    companyname: "Winter of Code,2020",
+    companyurl: "images/certificates/winterofcode.jpg",
+    date: "December,2020-January,2021",
+    description: [
+      "Contributed to Open Source Project",
+      "OMG Frames",
+      "Became TOP Contributor",
+    ],
+  },
+  {
+    position: "Hacktoberfest 2020",
+    companyname: "Hacktoberfest",
+    companyurl: "images/certificates/hacktoberfestpullrequests.jpg",
+    date: "October,2020-October,2020",
+    description: [
+      "Contributed First time in Open Source",
+      "Merged 4 Pull Requests",
+    ],
+  },
+  {
+    position: "GirlScript Summer of Code,2021",
+    companyname: "GirlScript Summer of Code",
+    companyurl: "images/certificates/girlscriptcertificate.png",
+    date: "March,2021-Present",
+    description: [
+      "Project Admin",
+      `<a href="https://github.com/sairish2001/makesmatheasy.github.io">Repository</a>`,
+    ],
+  },
+  {
+    position: "Let's Grow More Summer of Code,2021",
+    companyname: "Let's Grow More",
+    companyurl: null,
+    date: "July,2021-September,2021",
+    description: [
+      "Project Admin",
+      `<a href="https://github.com/sairish2001/makesmatheasy.github.io">Repository</a>`,
+    ],
+  },
+];
